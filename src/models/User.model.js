23d8bfc8f412af5a743f3bc94c06faef4907065d
lmaiorano <158139675+Pmaioranool@users.noreply.github.com>
2025-11-13 14:39:58 +0100

    feat(model): add User model

---
+++ src/models/User.model.js
@@ -0,0 +1,77 @@
+const { pool } = require("../config/db.postgres");
+const Workout = require("./models/Workout");
+const bcrypt = require("bcrypt");
+
+class User {
+  static async getAll() {
+    const res = await pool.query("SELECT * FROM users ORDER BY id");
+    return res.rows;
+  }
+
+  static async getById(id) {
+    const res = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    return res.rows[0] || null;
+  }
+
+  static async create({ name, email, password }) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const res = await pool.query(
+      "INSERT INTO users (name,email,password) VALUES ($1, $2) RETURNING *",
+      [name, email, hashedPassword]
+    );
+    return res.rows[0];
+  }
+
+  static async update(id, { name, email }) {
+    const res = await pool.query(
+      "UPDATE users SET name = $1, email = $2 WHERE id = $4 RETURNING *",
+      [name, email, id]
+    );
+    return res.rows[0] || null;
+  }
+
+  static async updatePassword(id, password) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const res = await pool.query(
+      "UPDATE users SET password = $1 WHERE id = $2 RETURNING *",
+      [hashedPassword, id]
+    );
+    return res.rows[0] || null;
+  }
+
+  static async updateLastLogin(id, lastLogin) {
+    const res = await pool.query(
+      "UPDATE users SET last_login = $1 WHERE id = $2 RETURNING *",
+      [lastLogin, id]
+    );
+    return res.rows[0] || null;
+  }
+
+  static async incrementWorkoutsCompleted(id, workoutId) {
+    const res = await pool.query(
+      "UPDATE users SET workouts_completed = workouts_completed + 1 WHERE id = $1 RETURNING *",
+      [id]
+    );
+
+    const user = res.rows[0];
+    if (!user) return null;
+
+    const workout = await Workout.findById(workoutId);
+    if (!workout) return null;
+
+    if (!workout.userId.includes(id)) {
+      workout.userId.push(id);
+    }
+
+    await workout.save();
+
+    return { user };
+  }
+
+  static async deleteById(id) {
+    await pool.query("DELETE FROM users WHERE id = $1", [id]);
+    return { deleted: true };
+  }
+}
+
+module.exports = User;
--- src/server.js
+++ src/server.js
@@ -57,7 +57,10 @@ app.get("/init-db", async (req, res) => {
       name VARCHAR(100) NOT NULL,
       email VARCHAR(255) NOT NULL UNIQUE,
       password VARCHAR(255) NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      workouts_completed INT DEFAULT 0,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      last_login TIMESTAMP NULL
     );`);
     res.json({ status: "Database initialized" });
   } catch (error) {
